266f643226d7a97285f5ba741e0d14c46dccfc1d
Liseth Gutierrez <liseth.yggdrasil@gmail.com>
2021-01-14 11:20:37 -0400

    first commit

---
+++ pages/blog/[slug].js
@@ -0,0 +1,72 @@
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import Link from 'next/link';
+import { getAllPostsWithSlug, getPost } from '../../lib/api';
+import styles from '../../styles/Home.module.css';
+import blogStyles from '../../styles/Blog.module.css';
+
+export default function Post({ postData }) {
+  const router = useRouter();
+
+  if(!router.isFallback && !postData?.slug) {
+    return <p>hmm... looks like an error</p>;
+  }
+
+  const formatDate = date => {
+    const newDate = new Date(date);
+
+    return `${newDate.getDate()}/
+      ${newDate.getMonth() + 1}/
+      ${newDate.getFullYear()}`;
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{postData.title}</title>
+        <link rel='icon' href='/favicon.ico'></link>
+      </Head>
+
+      <main className={styles.main}>
+        {router.isFallback ? (
+          <h2>Loading...</h2>
+        ) : (
+          <article className={blogStyles.article}>
+            <div className={blogStyles.postmeta}>
+              <h1 className={styles.title}>{postData.title}</h1>
+              <p>{formatDate(postData.date)}</p>
+            </div>
+            <div 
+              className='post-content content'
+              dangerouslySetInnerHTML= {{ __html: postData.content }}
+            />
+          </article>
+        )}
+        <p>
+          <Link href='/blog'>
+            <a>back to articles</a>
+          </Link>
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export const getStaticPaths = async() => {
+  const allPosts = await getAllPostsWithSlug();
+
+  return {
+    paths: allPosts.edges.map(({ node }) => `/blog/${node.slug}`) || [],
+    fallback: true
+  };
+}
+
+export const getStaticProps = async({ params }) => {
+  const data = await getPost(params.slug);
+
+  return {
+    props: {
+      postData: data.post
+    }
+  };
+}
---
+++ pages/blog/index.js
@@ -0,0 +1,45 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import { getAllPosts } from '../../lib/api';
+import styles from '../../styles/Home.module.css';
+import blogStyles from '../../styles/Blog.module.css';
+
+const Blog = ({ allPosts: { edges } }) => (
+  <div className={styles.container}>
+    <Head>
+      <title>Blog articles page</title>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+
+    <main className={styles.main}>
+      <h1 className={styles.title}>Latest blog articles</h1>
+      <hr />
+      <section>
+        {edges.map(({ node }) => (
+          <div className={blogStyles.listitem} key={node.id}>
+            <div className={blogStyles.listitem__content}>
+              <h2>{node.title}</h2>
+              <Link href={`/blog/${node.slug}`}>
+                <a>Read more</a>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
+  </div>
+);
+
+
+export const getStaticProps = async() => {
+  const allPosts = await getAllPosts();
+  return {
+    props: {
+      allPosts
+    }
+  };
+}
+
+
+export default Blog
+
