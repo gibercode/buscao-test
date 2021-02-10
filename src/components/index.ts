377174a5a87aebfc14db29bc72d98b7068546aa2
Victor Caceres <caceres.vc95@gmail.com>
2021-02-10 14:31:16 -0400

    create pagination component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
-export { default as Navbar } from './Navbar';
-export { default as FeaturedSlider } from './FeturedSlider';
-export { default as Card } from './Card';
-export { default as Footer } from './Footer';
+export { default as Navbar } from './Navbar'
+export { default as FeaturedSlider } from './FeturedSlider'
+export { default as Card } from './Card'
+export { default as Footer } from './Footer'
+export { default as Pagination } from './Pagination'
