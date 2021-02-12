c87064692f0c0d469fa9b7cd02777acb313e633c
Victor Caceres <caceres.vc95@gmail.com>
2021-02-11 21:23:35 -0400

    create pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,39 @@
+import React, { memo } from 'react'
+import { useState } from 'react'
 import styles from './styles.module.scss'
 
-const Pagination = () => (
-  <div>1</div>
-)
+const Pagination = ({ items, perPage, changePage }) => {
+  const [firstPage, setFirstPage] = useState(1)
 
-export default Pagination
+  const totalItems = items.length
+  const totalPages = Math.ceil(totalItems / perPage)
+
+  const [lastPage, setLastPage] = useState(totalPages > 5 ? 5 : totalPages)
+
+  const pageNumbers = []
+  for (let i = firstPage; i <= lastPage; i++) pageNumbers.push(i)
+
+  const determinatePages = (page) => {
+    if(page == lastPage && page != totalPages) changeNumberPages(1)
+    if(page == firstPage && page != 1) changeNumberPages(-1)
+
+    changePage(page)
+  }
+
+  const changeNumberPages = operator => {
+    setFirstPage(firstPage + operator)
+    setLastPage(lastPage + operator)
+  }
+
+  return (
+    <ul>
+      {
+        pageNumbers.map(pageNumber => (
+          <li key={pageNumber} onClick={() => determinatePages(pageNumber)}>{pageNumber}</li>
+        ))
+      }
+    </ul>
+  )
+}
+
+export default memo(Pagination)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import React, { FC } from 'react'
 import { AppProps } from 'next/app'
 import { wrapper } from '../store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { useStore } from 'react-redux'
 import '../../public/styles/globals.scss'
 
-const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
-)
+const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => {
+  const store = useStore()
+
+  return (
+    <PersistGate persistor={store.__persistor}>
+      <Component {...pageProps} />
+    </PersistGate>
+  )
+}
 
 export default wrapper.withRedux(WrappedApp)
