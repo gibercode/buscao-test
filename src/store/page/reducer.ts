c9f2fabd1adb0cf66f8de8d24660b8f777e7f679
Victor Caceres <caceres.vc95@gmail.com>
2021-02-12 18:45:24 -0400

    create page store files

---
+++ src/store/page/reducer.ts
@@ -0,0 +1,13 @@
+import { AnyAction } from 'redux'
+
+const initialState = {
+  homePage: {}
+}
+
+const pageReducer = (state = initialState, { type, payload }: AnyAction) => {
+  switch(type) {
+
+  }
+}
+
+export default pageReducer
