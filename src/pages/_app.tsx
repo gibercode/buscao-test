7f439b353793c0632b16cb09dae49808084a5634
Victor Caceres <caceres.vc95@gmail.com>
2021-02-12 21:38:31 -0400

    fix rehydrate store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
-import React, { FC } from 'react'
+import { FC, useEffect } from 'react'
 import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useStore } from 'react-redux'
 import { wrapper } from '../store'
 import '../../public/styles/globals.scss'
 
-const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
-)
+const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => {
+  const store = useStore()
+  const router = useRouter()
+
+  useEffect(() => {
+    if(router.route == '/') localStorage.clear()
+    store.__persistor.persist()
+  }, [])
+
+  return (
+    <Component {...pageProps} />
+  )
+}
 
 export default wrapper.withRedux(WrappedApp)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,9 @@
-import { useEffect } from 'react'
-import { useStore } from 'react-redux'
 import { getResources } from '../store/actions'
 import { wrapper } from '../store'
 import { Navbar } from '../components'
 import styles from '../../public/styles/Home.module.scss'
 
 const Home = () => {
-  const store = useStore()
-
-  useEffect(() => {
-    store.__persistor.purge()
-    store.__persistor.persist()
-  }, [])
-
   return (
     <div className={styles._container}>
       <Navbar />
