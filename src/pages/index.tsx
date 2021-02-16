8a48750e62123f37a6933c0809ca97a7b78a35d4
gibercode <giberlara@gmail.com>
2021-02-15 23:07:32 -0400

    new page for component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import { getResources } from '../store/actions'
 import { wrapper } from '../store'
-import { Navbar, Footer } from '../components'
+import { Navbar } from '../components'
 import styles from '../../public/styles/Home.module.scss'
 
 const Home = () => {
   return (
     <div className={styles._container}>
       <Navbar />
-      <Footer />
     </div>
   )
 }
