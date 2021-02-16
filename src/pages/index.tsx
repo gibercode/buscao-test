8796bf499bead7e3d89926ed676c6c973249b2c2
gibercode <giberlara@gmail.com>
2021-02-15 21:54:33 -0400

    new footer lay out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { getResources } from '../store/actions'
 import { wrapper } from '../store'
-import { Navbar } from '../components'
+import { Navbar, Footer } from '../components'
 import styles from '../../public/styles/Home.module.scss'
 
 const Home = () => {
   return (
     <div className={styles._container}>
       <Navbar />
+      <Footer />
     </div>
   )
 }
