944b9f9d8dbe10a4177ff2b6cdf33a5569be66da
gibercode <giberlara@gmail.com>
2021-02-16 21:50:59 -0400

    lay out progress/

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,12 +1,26 @@
 
+import { useRef } from 'react';
 import styles from './styles.module.scss';
 
+
 const Slider = () => {
+
+  const second = useRef();
   return (
     <div className={styles._sliderParent}>
+      <div className={styles._sliderChild}>
         <img src='images/backgrounds/blackpoints-background.svg' width='100%' height='100%' />
+        <img src='images/backgrounds/blackpoints-background.svg' width='100%' height='100%' ref={second}/>
         <img src='images/backgrounds/blackpoints-background.svg' width='100%' height='100%' />
-        <img src='images/backgrounds/blackpoints-background.svg' width='100%' height='100%' />
+      </div>
+
+      <div className={styles._stepper}>
+        <div className={styles._steps}>
+            <div className={styles._circle}></div>
+            <div className={styles._circle} ></div>
+            <div className={styles._circle}></div>
+        </div>
+      </div>
     </div>
   )
 };
