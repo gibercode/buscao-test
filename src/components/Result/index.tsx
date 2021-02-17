c415a42d3234d49d2d79416a12e6ae4b25654f25
Victor Caceres <caceres.vc95@gmail.com>
2021-02-17 19:59:32 -0400

    flex results

---
+++ src/components/Result/index.tsx
@@ -0,0 +1,32 @@
+import { memo, useState } from 'react'
+import { paginate } from '../../utils'
+import Card from '../Card'
+import Pagination from '../Pagination'
+import styles from './styles.module.scss'
+
+const perPage = 6
+
+const Result = ({ posts }) => {
+  const [page, setPage] = useState(1)
+
+  return (
+    <div className={styles._container}>
+      <div className={styles._cardContainer}>
+        {
+          paginate(posts, page, perPage).map((post, index) => {
+            return (
+              <div key={index} className={styles._cardSeparation}>
+                <Card name={post.title} />
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className={styles._paginationContainer}>
+        <Pagination currentPage={page} items={posts} perPage={perPage} changePage={setPage}/>
+      </div>
+    </div>
+  )
+}
+
+export default memo(Result)
--- src/pages/testv.tsx
+++ src/pages/testv.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import { Pagination, Card } from '../components'
-import { paginate } from '../utils'
+import { Result } from '../components'
 
 const posts = [
   { title: 'Local A' },
@@ -33,30 +31,9 @@ const posts = [
   { title: 'Local A' },
 ]
 
-const perPage = 6
-
 const Testv = () => {
-  const [page, setPage] = useState(1)
-
   return (
-    <>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <div style={{ display: 'flex', flexWrap: 'wrap', paddingRight: '20%', paddingLeft: '20%' }}>
-          {
-            paginate(posts, page, perPage).map((post, index) => {
-              return (
-                <div key={index} style={{ flex: '1 0 40%', margin: 25 }}>
-                  <Card name={post.title} />
-                </div>
-              )
-            })
-          }
-        </div>
-        <div style={{ display: 'flex', height: '10vh', justifyContent: 'center', alignItems: 'flex-end' }}>
-          <Pagination currentPage={page} items={posts} perPage={perPage} changePage={setPage}/>
-        </div>
-      </div>
-    </>
+    <Result posts={posts} />
   )
 }
 
