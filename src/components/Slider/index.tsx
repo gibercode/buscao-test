6c63378b819abf3dd23424a25973edc67064e42d
gibercode <giberlara@gmail.com>
2021-02-16 22:04:11 -0400

    slider progress

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -2,22 +2,38 @@
 import { useRef } from 'react';
 import styles from './styles.module.scss';
 
-
 const Slider = () => {
 
-  const second = useRef();
+  const slide = () => {
+    const getElement = document.getElementById('second');
+
+    if (getElement) {
+      getElement.scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
+  }
+
   return (
     <div className={styles._sliderParent}>
       <div className={styles._sliderChild}>
-        <img src='images/backgrounds/blackpoints-background.svg' width='100%' height='100%' />
-        <img src='images/backgrounds/blackpoints-background.svg' width='100%' height='100%' ref={second}/>
-        <img src='images/backgrounds/blackpoints-background.svg' width='100%' height='100%' />
+
+        <div className={styles._container}>
+         <img src='images/backgrounds/blackpoints-background.svg' width='100%'/>
+        </div>
+        <div className={styles._container} id='second'>
+         <img src='images/backgrounds/blackpoints-background.svg' width='100%'/>
+        </div>
+
+        <div className={styles._container} id='third'>
+         <img src='images/backgrounds/blackpoints-background.svg' width='100%'/>
+        </div>
       </div>
 
       <div className={styles._stepper}>
         <div className={styles._steps}>
             <div className={styles._circle}></div>
-            <div className={styles._circle} ></div>
+            <div className={styles._circle} onClick={slide} ></div>
             <div className={styles._circle}></div>
         </div>
       </div>
