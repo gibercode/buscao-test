cc8d87ac0d3e52951a0d80f11c5e461a0b7d73a7
MauriceMiot <maurice.yggdrasil@gmail.com>
2021-02-17 07:16:20 -0400

    adjustment coins

--- src/components/Welcome/welcome.tsx
+++ src/components/Welcome/welcome.tsx
@@ -6,7 +6,7 @@ import { Navbar, Search } from '..'
 const Welcome = () => {
 
   return (
-    <div>
+    <div >
       <div className={styles._main}>
         <Navbar />
         <div className={styles._container}>
@@ -18,60 +18,32 @@ const Welcome = () => {
               <p className={styles._description}>Bienvenido a la nueva era financiera, directorio de comercios activos que aceptan criptomonedas</p>
             </div>
           </div>
-        </div>
-        <div >
+          <div className={styles._searchBuild} >
          <Search />
         </div>
-
-     {/*    <div className={styles._searchContainer}>
-          <div className={styles._searchContent}>
-            <div className={styles._radioContainer}>
-              <input type='radio'></input>
-            </div>
-            <div className={styles._selectsContainer}>
-              <p className={styles._searchTitle}>UBICACIÓN</p>
-              <select name='Ubicacion' className={styles._selects} >
-                <option>Merida</option>
-              </select>
-            </div>
-            <div className={styles._radioContainer}>
-              <input type='radio'></input>
-            </div>
-            <div className={styles._selectsContainer}>
-              <p className={styles._searchTitle}>CATEGORIAS</p>
-              <select name='Categorias' className={styles._selects} >
-                <option>Caracas</option>
-              </select>
-            </div>
-            <div className={styles._inputContainer}>
-              <input placeholder='Que estás buscando' className={styles._input}></input>
-            </div>
-            <div className={styles._buttonContainer}>
-              <button className={styles._blueButtom}>Ir</button>
-            </div>
-          </div>
-        </div> */}
+        </div>
 
       </div>
-    {/*   <div>
+
+     <div>
         <div className={styles._blackfloor}>
           <div className={styles._coins}>
-            <div className={styles._coin}>
-              <img className={styles.coinposition8} src='../../images/lying-coins/lying-btc.svg' width='100%' ></img>
+            <div className={styles._coin1}>
+              <img className={styles.coinposition1} src='../../images/lying-coins/lying-btc.svg' width='100%' ></img>
             </div>
-            <div className={styles._coin}>
-              <img className={styles.coinposition3} src='../../images/lying-coins/lying-lite.svg' width='100%'  ></img>
+            <div className={styles._coin2}>
+              <img className={styles.coinposition2} src='../../images/lying-coins/lying-lite.svg' width='100%'  ></img>
             </div>
-            <div className={styles._coin}>
-              <img className={styles.coinposition4} src='../../images/lying-coins/lying-ether.svg' width='100%'></img>
+            <div className={styles._coin3}>
+              <img className={styles.coinposition3} src='../../images/lying-coins/lying-ether.svg' width='100%'></img>
             </div>
-            <div className={styles._coin}>
-              <img className={styles.coinposition} src='../../images/lying-coins/lying-xpt.svg' width='100%'></img>
+            <div className={styles._coin4}>
+              <img className={styles.coinposition4} src='../../images/lying-coins/lying-xpt.svg' width='100%'></img>
             </div>
 
           </div>
         </div>
-      </div> */}
+      </div>
     </div>
 
   )
