7fb8f745080e24f937a5d669c388803ca9c3fa21
gibercode <giberlara@gmail.com>
2021-02-17 23:15:46 -0400

    slider progress

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,40 +1,68 @@
 
-import { useRef } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
+
+const images = [
+  { path: 'http://admin-buscao.thecodeworkers.com/wp-content/uploads/2021/02/pay-background.png', id: 'first' },
+  { path: 'images/backgrounds/blackpoints-background.svg', id: 'second' },
+  { path: 'images/backgrounds/blackpoints-background.svg', id: 'third' }
+];
 
 const Slider = () => {
 
-  const slide = () => {
-    const getElement = document.getElementById('second');
+  const [currentImage, setCurrentImage] = useState(0);
+  const [up, setUp] = useState(true);
+
+  useEffect(() => {
+    sliding();
+    if (currentImage == 2) setUp(false);
+    if (currentImage == 1 && !up) setUp(true);
+  }, [currentImage])
+
+  const sliding = () => {
+    const getElement = document.getElementById(images[currentImage].id);
 
     if (getElement) {
       getElement.scrollIntoView({
         behavior: 'smooth',
       });
     }
+
+    setTimeout(() => {
+      if (currentImage >= 0 && currentImage < 2 && up) {
+        return setCurrentImage(currentImage + 1);
+      }
+      if (currentImage <= 2) {
+        if (currentImage >= 1) setCurrentImage(currentImage - 1);
+        return;
+      }
+    }, 4000);
+  }
+
+  const checkStep = (step) => {
+    if (currentImage === step) return styles._currentCircle;
+    return styles._circle;
   }
 
   return (
     <div className={styles._sliderParent}>
       <div className={styles._sliderChild}>
-
-        <div className={styles._container}>
-         <img src='images/backgrounds/blackpoints-background.svg' width='100%'/>
-        </div>
-        <div className={styles._container} id='second'>
-         <img src='images/backgrounds/blackpoints-background.svg' width='100%'/>
-        </div>
-
-        <div className={styles._container} id='third'>
-         <img src='images/backgrounds/blackpoints-background.svg' width='100%'/>
-        </div>
+        {
+          images.map((image, index) => {
+            return (
+              <div className={styles._container} key={index}>
+                <img src={image.path} width='100%' id={image.id} />
+              </div>
+            )
+          })
+        }
       </div>
 
       <div className={styles._stepper}>
         <div className={styles._steps}>
-            <div className={styles._circle}></div>
-            <div className={styles._circle} onClick={slide} ></div>
-            <div className={styles._circle}></div>
+          <div className={checkStep(0)}></div>
+          <div className={checkStep(1)}></div>
+          <div className={checkStep(2)}></div>
         </div>
       </div>
     </div>
