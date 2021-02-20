32b83665cd21f3bcf4fd4a16ea8371aaf3270359
MauriceMiot <maurice.yggdrasil@gmail.com>
2021-02-19 21:12:30 -0400

    adjustment scss

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -5,77 +5,77 @@ import { Checkbox, DropDown } from '../../../public/images/icons'
 import styles from './styles.module.scss'
 
 const Search = () => {
-    const dispatch = useDispatch()
-    const [checkedOne, setCheckedOne]: any = useState(false)
-    const [checkedTwo, setCheckedTwo]: any = useState(false)
-    const { categories, currentStates } = useSelector(state => state.resource);
-    const [filter, setFilter] = useState(
-        {
-            category: '',
-            title: ''
-        }
-    );
-
-    const changeState = (event) => {
-        dispatch(filterPosts(event.target.value, 'state'))
-        setFilter({ ...filter, ...{ category: '', title: '' } })
+  const dispatch = useDispatch()
+  const [checkedOne, setCheckedOne]: any = useState(false)
+  const [checkedTwo, setCheckedTwo]: any = useState(false)
+  const { categories, currentStates } = useSelector(state => state.resource);
+  const [filter, setFilter] = useState(
+    {
+      category: '',
+      title: ''
     }
+  );
 
-    const checkStateOne = () => {
-      setCheckedOne(!checkedOne)
-    }
-    const checkStateTwo = () =>{
-      setCheckedTwo(!checkedTwo)
-    }
+  const changeState = (event) => {
+    dispatch(filterPosts(event.target.value, 'state'))
+    setFilter({ ...filter, ...{ category: '', title: '' } })
+  }
 
-    const changeCategory = (event) => {
-        dispatch(filterPosts(event.target.value, 'categories'))
-        setFilter({ ...filter, ...{ category: event.target.value, title: '' } })
-    }
+  const checkStateOne = () => {
+    setCheckedOne(!checkedOne)
+  }
+  const checkStateTwo = () => {
+    setCheckedTwo(!checkedTwo)
+  }
 
-    const changeTitle = (event) => {
-        dispatch(filterPosts(filter.title, 'title'))
-    }
+  const changeCategory = (event) => {
+    dispatch(filterPosts(event.target.value, 'categories'))
+    setFilter({ ...filter, ...{ category: event.target.value, title: '' } })
+  }
+
+  const changeTitle = (event) => {
+    dispatch(filterPosts(filter.title, 'title'))
+  }
 
 
-    return (
-        <div className={styles._searchContent}>
-            <div className={styles._selectContainer}>
-            <div className={styles._radioContainer} onClick={checkStateOne}>
-              <Checkbox color={checkedOne ? '#1652F0' : '#93959A'} />
-            </div>
-                <div className={styles._select}>
-                  <label style={{display: 'flex'}}>
-                    <label  htmlFor='state'>UBICACION</label>
-                   <div className={styles._dropdown}> <DropDown /> </div>
-                    </label>
-                    <select name='state' onChange={changeState}>
-                        <option value=''>Todos</option>
-                        {currentStates.map((state, index) => (<option value={state.slug} key={index}>{state.name}</option>))}
-                    </select>
-                </div>
-            </div>
-            <div className={styles._selectContainer}>
-            <div className={styles._radioContainer} onClick={checkStateTwo}>
-            <Checkbox color={checkedTwo ? '#1652F0' : '#93959A'} />
-            </div>
-                <div className={styles._select}>
-                <label style={{display: 'flex'}}>
-                    <label htmlFor='category'>CATEGORIAS</label>
-                    <div className={styles._dropdown}> <DropDown /> </div>
-                    </label>
-                    <select name='category' value={filter.category} onChange={changeCategory} >
-                        <option value=''>Todos</option>
-                        {categories.map((category, index) => (<option value={category.slug} key={index}>{category.name}</option>))}
-                    </select>
-                </div>
-            </div>
-            <div className={styles._inputContainer}>
-                <input placeholder='Que estas buscando' value={filter.title} onChange={(event) => setFilter({ ...filter, ...{ title: event.target.value } })} />
-                <button className={styles._goButton} onClick={changeTitle}>Ir</button>
-            </div>
+  return (
+    <div className={styles._searchContent}>
+      <div className={styles._selectContainer}>
+        <div className={styles._radioContainer} onClick={checkStateOne}>
+          <Checkbox color={checkedOne ? '#1652F0' : '#93959A'} />
         </div>
-    )
+        <div className={styles._select}>
+          <label style={{ display: 'flex' }}>
+            <label htmlFor='state'>UBICACION</label>
+            <div className={styles._dropdown}> <DropDown /> </div>
+          </label>
+          <select name='state' onChange={changeState}>
+            <option value=''>Todos</option>
+            {currentStates.map((state, index) => (<option value={state.slug} key={index}>{state.name}</option>))}
+          </select>
+        </div>
+      </div>
+      <div className={styles._selectContainer}>
+        <div className={styles._radioContainer} onClick={checkStateTwo}>
+          <Checkbox color={checkedTwo ? '#1652F0' : '#93959A'} />
+        </div>
+        <div className={styles._select}>
+          <label style={{ display: 'flex' }}>
+            <label htmlFor='category'>CATEGORIAS</label>
+            <div className={styles._dropdown}> <DropDown /> </div>
+          </label>
+          <select name='category' value={filter.category} onChange={changeCategory} >
+            <option value=''>Todos</option>
+            {categories.map((category, index) => (<option value={category.slug} key={index}>{category.name}</option>))}
+          </select>
+        </div>
+      </div>
+      <div className={styles._inputContainer}>
+        <input placeholder='Que estas buscando' value={filter.title} onChange={(event) => setFilter({ ...filter, ...{ title: event.target.value } })} />
+        <button className={styles._goButton} onClick={changeTitle}>Ir</button>
+      </div>
+    </div>
+  )
 }
 
 export default Search;
