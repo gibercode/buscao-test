f0450798a2c91b8ad2b634c2d7d2c7553975320d
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-02-23 07:59:31 -0400

    No exists states - no filter

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -40,28 +40,30 @@ const Search = () => {
 
   return (
     <div className={styles._searchContent}>
-      <div className={styles._selectContainer}>
-        <div className={styles._radioContainer} onClick={checkStateOne}>
-          <Checkbox color={checkedOne ? '#1652F0' : '#93959A'} />
+      {currentStates.length ? (
+        <div className={styles._selectContainer}>
+          <div className={styles._radioContainer} onClick={checkStateOne}>
+            <Checkbox color={checkedOne ? '#1652F0' : '#93959A'} />
+          </div>
+          <div className={styles._select}>
+            <label style={{ display: 'flex' }}>
+              <label style={{ color: checkedOne ? '#1652F0' : '#93959A' }} htmlFor='state'>UBICACIÓN</label>
+              <div className={styles._dropdown}> <DropDown color={checkedOne ? '#1652F0' : '#93959A'} /> </div>
+            </label>
+            <select name='state' onChange={changeState}>
+              <option value=''>Todos</option>
+              {currentStates.map((state, index) => (<option value={state.slug} key={index}>{state.name}</option>))}
+            </select>
+          </div>
         </div>
-        <div className={styles._select}>
-          <label style={{ display: 'flex' }}>
-            <label style={{color: checkedOne ? '#1652F0' : '#93959A'}} htmlFor='state'>UBICACIÓN</label>
-            <div className={styles._dropdown}> <DropDown color={checkedOne ? '#1652F0' : '#93959A'} /> </div>
-          </label>
-          <select name='state' onChange={changeState}>
-            <option value=''>Todos</option>
-            {currentStates.map((state, index) => (<option value={state.slug} key={index}>{state.name}</option>))}
-          </select>
-        </div>
-      </div>
+      ) : null}
       <div className={styles._selectContainer}>
         <div className={styles._radioContainer} onClick={checkStateTwo}>
           <Checkbox color={checkedTwo ? '#1652F0' : '#93959A'} />
         </div>
         <div className={styles._select}>
           <label style={{ display: 'flex' }}>
-            <label style={{color: checkedTwo ? '#1652F0' : '#93959A'}} htmlFor='category'>CATEGORIAS</label>
+            <label style={{ color: checkedTwo ? '#1652F0' : '#93959A' }} htmlFor='category'>CATEGORIAS</label>
             <div className={styles._dropdown}> <DropDown color={checkedTwo ? '#1652F0' : '#93959A'} /> </div>
           </label>
           <select name='category' value={filter.category} onChange={changeCategory} >
